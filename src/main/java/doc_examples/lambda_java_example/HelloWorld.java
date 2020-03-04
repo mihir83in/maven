2eca37c3d6b6e80badc664d17a31d70044e2ad86
Pawan Gamage <alexandritegalle@gmail.com>
2020-03-04 12:18:59 +1100

    first Commit

---
+++ src/main/java/doc_examples/lambda_java_example/HelloWorld.java
@@ -0,0 +1,14 @@
+package doc_examples.lambda_java_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	public String myHandler() {
+	    return "Hello World from lambda update test 1.1";
+	  }
+}
+
+
